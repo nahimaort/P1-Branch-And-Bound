7d5626d178b77de1a9726750c5f3df42818f0859
nahimaort <nahimaortega@gmail.com>
2021-10-16 14:06:25 +0100

    Práctica 1.1 versión 1.1

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ print(search.breadth_first_graph_search(ab).path())
 print(search.depth_first_graph_search(ab).path())
 print(search.branch_and_bound_graph_search(ab).path())
 
+
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
 # [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
